bc140f168601b8cc27a19bff2cf073e198227e02
tplamondon <tplamond@sfu.ca>
2019-04-13 15:29:53 -0700

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+You will have to download JIntellitype.dll yourself and place in in /src to compile this
---
+++ src/nekofactory/autoclicker/Main.java
@@ -0,0 +1,28 @@
+package nekofactory.autoclicker;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//https://www.chilkatsoft.com/java-loadlibrary-windows.asp
+		try {
+	    	File file = new File("src/Jintellitype.dll");
+	    	String pathLocation = file.getAbsolutePath();
+	    	System.load(pathLocation);
+	    	
+	    } catch (UnsatisfiedLinkError e) {
+	      System.err.println("Native code library failed to load.\n" + e);
+	      System.exit(1);
+	    }
+		System.out.println("Succesfully loaded library");
+		
+		
+	}//end main method
+
+}//end class
