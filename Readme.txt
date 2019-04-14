7ddb1df9ed84d91561d16eeb9bfac19bf84f1467
tplamondon <tplamond@sfu.ca>
2019-04-13 18:37:56 -0700

    got one hotkey to work

--- Readme.txt
+++ Readme.txt
@@ -2,3 +2,9 @@ You will have to add the JIntellitype library yourself if you want this to run
 
 In Eclipse, right click your project > properties > Java Build Path > Libraries > Add Library > Add User Library
 Name it JIntellitype, add the Jar you download
+
+
+
+Another possibly library?
+https://github.com/kwhat/jnativehook
+https://stackoverflow.com/questions/458756/java-system-wide-keyboard-shortcut
--- src/autoclicker/Main.java
+++ src/autoclicker/Main.java
@@ -1,7 +1,9 @@
 package autoclicker;
 
-import java.io.File;
+import java.awt.AWTException;
+import java.awt.Robot;
 
+import com.melloware.jintellitype.HotkeyListener;
 import com.melloware.jintellitype.JIntellitype;
 
 
@@ -13,7 +15,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		//https://www.chilkatsoft.com/java-loadlibrary-windows.asp
-		try {
+		/*try {
 			File file = new File("dll/Jintellitype.dll");
 	    	String pathLocation = file.getAbsolutePath();
 	    	System.load(pathLocation);
@@ -21,11 +23,59 @@ public class Main {
 	    } catch (UnsatisfiedLinkError e) {
 	      System.err.println("Native code library failed to load.\n" + e);
 	      System.exit(1);
-	    }
+	    }*/
+		
+		// next check to make sure JIntellitype DLL can be found and we are on
+		// a Windows operating System
+		if (!JIntellitype.isJIntellitypeSupported()) {
+			System.exit(1);
+		}
 	    
 		System.out.println("Succesfully loaded library");
+		
 
-		JIntellitype.getInstance().registerHotKey(1, JIntellitype.MOD_WIN, (int)'A');
+		
+		JIntellitype.getInstance().addHotKeyListener(new HotkeyListener(){
+			public void onHotKey(int aIdentifier) {
+				if(aIdentifier == 1){
+					System.out.println("WINDOWS+A hotkey pressed");
+				}	
+				if(aIdentifier == 2){
+					System.out.println("CTRL+ALT+P hotkey pressed");
+				}	
+				System.out.println("WM_HOTKEY message received " + Integer.toString(aIdentifier));
+			}
+			
+		});
+		
+		/*JIntellitype.getInstance().addIntellitypeListener(new IntellitypeListener() {
+			public void onIntellitype(int aCommand) {
+		        switch (aCommand) {
+		            case JIntellitype.APPCOMMAND_MEDIA_PLAY_PAUSE:
+		                System.out.println("Play/Pause message received " + Integer.toString(aCommand));
+		                break;
+		            case JIntellitype.MOD_WIN: //same as APPCOMMAND_VOLUME_MUTE?a
+		            	System.out.println("This is volume mute key for some reason on my keyboard");
+		            	break;
+		            default:
+		                System.out.println("Undefined INTELLITYPE message caught " + Integer.toString(aCommand));
+		                break;
+		        }
+			}
+		});*/
+		
+		JIntellitype.getInstance().registerHotKey(1, JIntellitype.MOD_WIN, 'A');
+		JIntellitype.getInstance().registerHotKey(2, JIntellitype.MOD_CONTROL + JIntellitype.MOD_ALT, 'P');	
+		
+		System.out.println("Hotkeys registered");
+		
+		//add a robot
+		try {
+			Robot robot = new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		
 	}//end main method
 
 }//end class
