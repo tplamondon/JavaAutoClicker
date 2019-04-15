a2a008139f313df07db6110f8e81b9b67d95d95c
tplamondon <tplamond@sfu.ca>
2019-04-15 14:44:14 -0700

    removed unneeded imports

--- src/autoclicker/AutoClickerMain.java
+++ src/autoclicker/AutoClickerMain.java
@@ -5,11 +5,7 @@ import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
 
 import com.melloware.jintellitype.HotkeyListener;
